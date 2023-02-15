cc19f50c777c192109e7059d462ed584bb77f6cb
john <kiokoj81@gmail.com>
2023-02-15 21:38:47 +0300

    fix linter errors

--- spec/views/user/index_spec.rb
+++ spec/views/user/index_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 RSpec.describe 'Users', type: :feature do
   describe 'index page' do
     before(:each) do
-      @first_user = User.create(name: 'Harry njuguna',
-                                photo: 'https://wallpapers.com/images/featured/2zmiybnpu693b69o.jpg', bio: 'Teacher from USA.')
-      @second_user = User.create(name: 'John K',
-                                 photo: 'https://i.pinimg.com/736x/b1/1a/7e/b11a7e8ad1ef52c852b64d1c8ebf7f97.jpg', bio: 'Teacher from Poland')
+      @first_user = User.create(name: 'Monkey D Luffy', photo: 'https://wallpapers.com/images/featured/2zmiybnpu693b69o.jpg', bio: 'The guy who is gonna become the next pirate king.')
+      @second_user = User.create(name: 'Roronoa Zoro', photo: 'https://i.pinimg.com/736x/b1/1a/7e/b11a7e8ad1ef52c852b64d1c8ebf7f97.jpg',
+                                 bio: 'The guy who is gonna become the worlds greatest Swordsmen')
 
-      @first_post = Post.create(author: @first_user, title: 'A journey of a thousand miles ',
-                                text: 'Welcome to team. We\'re heading to the moon')
+      @first_post = Post.create(author: @first_user, title: 'Journey to the Grand Line', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed congue tortor vel varius.')
+      @second_post = Post.create(author: @second_user, title: 'Training with Mihawk', text: 'Donec eget mauris consectetur, laoreet libero ac, porttitor lacus.')
+
       @users = User.all
     end
     scenario 'can see the username of all other users.' do
